e70418fca479035c4ab5912d9ffe1df7bb4d8754
Eduardo Henrique <42817682+EduardoHenrique18@users.noreply.github.com>
2019-06-02 23:54:46 -0300

    Add files via upload

---
+++ js/scriptadmlivros.js
@@ -0,0 +1,80 @@
+function check(form) {
+  if (form.Login.value === "dev" && form.password === "1123") {
+    window.open('www.google.com')
+  } else {
+    alert("errado")
+  }
+}
+
+//Search
+$(document).ready(function() {
+  $("#Search").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#dataTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+//Converte a data
+function timeconvert(ds) {
+  var D, dtime, T, tz, off,
+    dobj = ds.match(/(\d+)|([+-])|(\d{4})/g);
+  T = parseInt(dobj[0]);
+  tz = dobj[1];
+  off = dobj[2];
+  if (off) {
+    off = (parseInt(off.substring(0, 2), 10) * 3600000) +
+      (parseInt(off.substring(2), 10) * 60000);
+    if (tz == '-') off *= -1;
+  } else off = 0;
+  return new Date(T += off).toUTCString();
+}
+
+function exibirResultado(id, dados) {
+  console.log(typeof dados === 'object')
+  console.log(typeof dados === 'string')
+  console.log(typeof dados === 'array')
+  console.log(dados)
+
+  //texto.forEach(function (o,index) {
+  //console.log(o.CpfUsuario)
+}
+
+$(document).ready(function() {
+  $.ajax({
+    url: 'http://localhost:56795/BibliotecaService.svc/ListarTodosLivros',
+    method: "GET",
+    crossDomain: true,
+    success: function(data) {
+      exibirResultado('abc', data);
+      $(data.ListarTodosLivrosResult).each(function(index, value) {
+        var record = "<tr><td>" + value.CodLivro + "</td><td>" + value.TituloLivro + "</td><td>" + value.Autor + "</td><td>" +
+        value.EditoraLivro + "</td><td>" + value.Situaçao +"</dt></tr>";
+        $("Table").append(record);
+      });
+    },
+    error: function(err) {
+      alert(err);
+      console.log('erro');
+
+    }
+  });
+});
+
+/*
+function confirmarDevolucao() {
+  var id = document.getElementById("Confirmar").value;
+  var myObj = {
+    "Id_Aluguel": id
+  };
+  var jsondata = JSON.stringify(myObj);
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:56795/BibliotecaService.svc/ConfirmarEntrega",
+    data: jsondata,
+    dataType: "json",
+    success: function() {
+      alert('pop the champagne');
+    }
+  });
+}*/
